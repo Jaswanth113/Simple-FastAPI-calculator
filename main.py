27d766d3f2b128aaef234bc63198ddba7e1d08be
Jaswanth Tikkireddy <143773331+Jaswanth113@users.noreply.github.com>
2025-06-27 17:48:17 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/addition")
+def addition(a: float, b:float):
+    result = a + b
+    return(result)
+
+@app.get("/subtraction")
+def subtraction(a: float, b: float):
+    result = a - b
+    return(result)
+
+@app.get("/multiplication")
+def multiplication(a:float, b:float):
+    result = a * b
+    return(result)
+
+@app.get("/division")
+def division(a:float, b:float):
+    if b == 0:
+        return("cannot be divited by zero")
+    result = a/b
+    return(result)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app")
